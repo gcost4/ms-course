41b67012885d8b5df9f33678bf9f73afa7a7f15c
Gustavo Costa <guucoostaa@hotmail.com>
2024-03-13 15:10:47 -0300

    hr-implementation

---
+++ hr-worker/src/main/java/com/gcost4/hrworker/EnableEurekaClient.java
@@ -0,0 +1,5 @@
+package com.gcost4.hrworker;
+
+public @interface EnableEurekaClient {
+
+}
--- hr-worker/src/main/java/com/gcost4/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/gcost4/hrworker/HrWorkerApplication.java
@@ -3,6 +3,8 @@ package com.gcost4.hrworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
